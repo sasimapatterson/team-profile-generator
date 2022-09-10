d00da2531612126f75134601f1c7c64c2cd260b5
Sasi Patterson <sweetpea_th@hotmail.com>
2022-09-10 14:58:51 -0400

    did the Employee.test.js and got it to pass.

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,5 +1,41 @@
 const Employee = require("../lib/Employee");
 
 describe("Employee", () => {
+    describe("Initialization", () => {
+        //This should return an object which contains method to get name, id and email. 
+        it("should return an object", () => {
+            
+            const obj = new Employee()
+            // expect(typeof(obj)).toEqual("object");
+            expect("name", "id", "email" in obj).toEqual(true);
+        });
+    });
+
+    describe ("name", () => {
+        //this will return the name from the getName() which is created inside Employee.js
+        it("should create new name", () => {
+
+            const obj = new Employee("Rachel");
+            expect(obj.name).toEqual("Rachel");
+        });
+    });
+
+    describe ("Id", () => {
+        //this will return id from the getId() which is created inside Employee.js
+        it("should create new id", () => {
+
+            const obj = new Employee("Rachel", 123);
+            expect(obj.id).toEqual(123);
+        });
+    });
+
+    describe ("email", () => {
+        //this will return e-mail from the getEmail() which is created inside Employee.js
+        it("should create new id", () => {
+
+            const obj = new Employee("Rachel", 123, "rachel@friends.com");
+            expect(obj.email).toEqual("rachel@friends.com");
+        });
+    });
 
 });
